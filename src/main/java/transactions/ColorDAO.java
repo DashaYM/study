4f1d9bb4123dcb3b7f54ee4e0ea7e32eab908ef0
dasha <dy.minina@gmail.com>
2018-10-23 10:53:19 +0700

    transactions on container level

--- src/main/java/transactions/ColorDAO.java
+++ src/main/java/transactions/ColorDAO.java
@@ -1,23 +1,18 @@
 package transactions;
 
-import javax.ejb.Stateless;
+import javax.ejb.*;
 import javax.persistence.*;
 import java.util.List;
 
 @Stateless
 public class ColorDAO {
 
-    private static final String PERSISTENCE_UNIT_NAME = "TestPersistence";
-    private static final EntityManagerFactory emFactoryObj = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-    // This Method Is Used To Retrieve The 'EntityManager' Object
+    @PersistenceContext(unitName = "TestPersistence")
+    EntityManager em;
 
-    public static EntityManager em = emFactoryObj.createEntityManager();
-
-    public Colour simpleMerge(Colour entity) {
-        em.getTransaction().begin();
+    public void simpleMerge(Colour entity) {
         em.persist(entity);
-        em.getTransaction().commit();
-        return entity;
+        em.flush();
     }
 
     public List<Colour> findAll() {
